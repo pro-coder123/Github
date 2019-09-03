7143dd43d0fabdb79fbebd5df456c48492a68dfc
cgi <varun.mukundhan@gmail.com>
2019-09-03 14:39:18 +0530

    With modal

--- src/app/repositories/repositories.component.ts
+++ src/app/repositories/repositories.component.ts
@@ -28,8 +28,10 @@ export class RepositoriesComponent implements OnInit {
   public clicked : any;
   public isclicked = false;
 
-  public toDelete : string;
+  public chosen : string;
+  public chosen_obj : any;
   
+  public favourite : any;
 
   
   ngOnInit() {
@@ -39,9 +41,14 @@ export class RepositoriesComponent implements OnInit {
       this.res = res;
     });
     
+    this.http.get("http://localhost:3000/favourites").subscribe(x => {
+      this.favourite = x;
+    console.log(x);
+    }
+      );
   }
   options = {
-    headers: new HttpHeaders({ 'Authorization': 'token ' })
+    headers: new HttpHeaders({ 'Authorization': 'token 0f0e44cb78c99ca12c529126a62ba5cb756bd7ab' })
   }
 
   
@@ -57,7 +64,7 @@ export class RepositoriesComponent implements OnInit {
     };
     
     this.http.post(this._url, this.myObj, this.options).subscribe(s => {
-      this.res.push(s);
+      this.res.push(s); 
     });
 
   }
@@ -68,20 +75,32 @@ export class RepositoriesComponent implements OnInit {
     this.clicked=reps;
     this.isclicked=true;
   }
-  set(value)
+  set(value,obj)
   {
-    this.toDelete = value;
+    this.chosen = value;
+    this.chosen_obj=obj;
   }
   del()
   {
-    console.log(this.toDelete);
-    let delete_url="https://api.github.com/repos/pro-coder123/"+this.toDelete;
+    console.log(this.chosen);
+    let delete_url="https://api.github.com/repos/pro-coder123/"+this.chosen;
     this.http.delete(delete_url,this.options).subscribe(x => {
 
-      this.res = this.res.filter(item => item.name !== this.toDelete);
-
+      this.res = this.res.filter(item => item.name !== this.chosen);
+      this.favourite = this.favourite.filter(item => item.id !== this.chosen_obj.id);
     });
 
+  }
+  add()
+  {
+    let add_url = "http://localhost:3000/favourites";
+    let options = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json'})
+
+    }
+    this.http.post(add_url,this.chosen_obj,options).subscribe(s => {
+      this.favourite.push(s);
+    });
   }
   Search(_search)
   {
@@ -91,5 +110,21 @@ export class RepositoriesComponent implements OnInit {
       this.http.get(this.search_url).subscribe(res => {this.search_results=res;});
       this.test=true;
      
+  }
+
+
+  remove()
+  {
+    let del_url="http://localhost:3000/favourites/"+this.chosen_obj.id;
+    let opt = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json'})
+
+    }
+    this.http.delete(del_url,opt).subscribe(x => {
+
+      this.favourite = this.favourite.filter(item => item.id !== this.chosen_obj.id);
+
+    });
+
   }
 }
